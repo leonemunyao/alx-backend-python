7aca8490017f3170cfd85bd05732940c8cc58f10
Leone Munyao <leonemunyao@gmail.com>
2025-07-21 12:38:49 +0300

    Implementing Authentication. Adding user authentication using JWT. Ensuring all users can access their own messages and conversations

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -136,6 +136,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
