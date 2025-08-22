a1f7350f7e065ba700d9af1eb2fa3399843a8bab
Leone Munyao <leonemunyao@gmail.com>
2025-08-22 13:55:54 +0300

    Building a docker image for the messaging app using Jenkins.

--- messaging_app/messaging_app/test_settings.py
+++ messaging_app/messaging_app/test_settings.py
@@ -20,8 +20,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'chats',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
